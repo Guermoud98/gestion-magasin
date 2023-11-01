b106564ab0a2b7c636f559f935e67ea44497d070
Gur <guermoud.@gmail.com>
2023-11-01 21:38:58 +0100

    Added the Java Magasin project and created the Product class

---
+++ magasin/src/magasin/Produit.java
@@ -0,0 +1,15 @@
+package magasin;
+
+import java.util.*;
+
+public class Produit {
+	private long id;
+	private String designation;
+	private int qte;
+	private double prix;
+	private Date date;
+	/* public Date(int year,
+              int month,
+              int date)
+     */
+}
