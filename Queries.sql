99518f826a95f7f51ef88ce9b6767ddb39b74978
Gur <guermoud.@gmail.com>
2023-11-07 22:09:31 +0100

    Created a 'Client' table in my 'gestionMagasin1' database

--- Queries.sql
+++ Queries.sql
@@ -1,4 +1,5 @@
 USE gestionMagasin1;
+--produit 
 CREATE TABLE produit (
 	id int PRIMARY KEY IDENTITY(1,1),
 	designation varchar(20),
@@ -7,3 +8,14 @@ CREATE TABLE produit (
 	date Date
 );
 SELECT * FROM produit;
+
+-- Client
+CREATE TABLE Client (
+    id int PRIMARY KEY IDENTITY(1,1),
+	nom varchar(10),
+	prenom varchar(15),
+	telephone varchar(10),
+	email varchar(20),
+	adresse varchar(20)
+);
+SELECT * FROM client;
