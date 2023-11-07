14667cd6c078a553e13a6af3994471a5e633a59a
Gur <guermoud.@gmail.com>
2023-11-07 22:20:29 +0100

    Implemented the IClientDao interface in the package clients

--- magasin/src/clients/IClientDao.java
+++ magasin/src/clients/IClientDao.java
@@ -1,5 +1,9 @@
 package clients;
 
 public interface IClientDao {
-
+	public void add(Client cl);
+	public Client search(int id);
+	public void modify(int id);
+	public void delete(int id);
+	public Client getAll();
 }
