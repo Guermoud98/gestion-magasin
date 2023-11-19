d69b57b2c38e53256b00c706fd26b03bbc84b20a
Gur <guermoud.@gmail.com>
2023-11-19 23:43:40 +0100

    Added the interface IVenteDao to the vente package

--- magasin/src/vente/IVenteDao.java
+++ magasin/src/vente/IVenteDao.java
@@ -1,5 +1,16 @@
 package vente;
 
+import java.util.*;
+
 public interface IVenteDao {
+	public void add(Vente v);
+	public void search(int id);
+	public List<Vente> display();
+	public void modify(int id);
+	public void delete(int id);
+	public double TotalLigneCommande(List<LigneCommande> ligneC);
+	public boolean isExistLigneComm(LigneCommande ligneC);
+	
+	
 
 }
